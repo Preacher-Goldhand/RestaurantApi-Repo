7f42984617ba7c56e0278616355f27a52c563f2f
Mateusz Hanulok <matehan97@gmail.com>
2022-04-01 12:22:05 +0200

    controllers of api

---
+++ Controllers/RestaurantController.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestaurantAPI.Entities;
+using RestaurantAPI.Models;
+using RestaurantAPI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Controllers
+{
+    [Route("api/restaurant")]
+    public class RestaurantController : ControllerBase
+    {
+        private IRestaurantService _restaurantService;
+
+        public RestaurantController(IRestaurantService restaurantService)
+        {
+            _restaurantService = restaurantService;
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete([FromRoute] int id)
+        {
+            _restaurantService.Delete(id);
+
+            return NoContent(); 
+        }
+        [HttpPost]
+        public ActionResult CreateRestaruant([FromBody] CreateRestaurantDto dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var id = _restaurantService.Create(dto);
+
+            return Created($"/api/restaurant/{id}", null);
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<RestaurantDto>> GetAll()
+        {
+            var restaurantsDtos = _restaurantService.GetAll();
+            return Ok(restaurantsDtos);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Restaurant> Get([FromRoute] int id)
+        {
+            var restaurant = _restaurantService.GetById(id);
+
+            return Ok(restaurant);
+        }
+        [HttpPut("{id}")]
+        public ActionResult Update([FromBody] UpdateRestaurantDto dto, [FromRoute] int id)
+        {
+            if (!ModelState.IsValid) { return BadRequest(ModelState); }
+
+            _restaurantService.Update(id, dto);
+           
+            return Ok();   
+        }
+
+    }
+  
+}
